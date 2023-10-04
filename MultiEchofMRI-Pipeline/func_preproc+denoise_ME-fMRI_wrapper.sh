fdf12a7805cabcd80d1c454405f5b50956a2f6a1
shotgunosine <adenosine@gmail.com>
2023-10-04 09:57:22 -0400

    update paths, add command echos

--- MultiEchofMRI-Pipeline/func_preproc+denoise_ME-fMRI_wrapper.sh
+++ MultiEchofMRI-Pipeline/func_preproc+denoise_ME-fMRI_wrapper.sh
@@ -19,12 +19,26 @@ if [ "${StudyFolder: -1}" = "/" ]; then
 	StudyFolder=${StudyFolder%?};
 fi
 
+# setup mcr
+ver=v93
+mcr=/usr/local/matlab-compiler/${ver}.tar.gz
+[[ -e "$mcr" ]] || exit 100
+[[ -d "/lscratch/$SLURM_JOB_ID" ]] || exit 101
+
+tar -C /lscratch/$SLURM_JOB_ID -xzf "$mcr"
+
+
 # define subject directory;
 Subdir="$StudyFolder"/"$Subject"
 
 # define some directories containing 
 # custom matlab scripts and various atlas files;
-MEDIR="/home/charleslynch/MultiEchofMRI-Pipeline"
+MEDIR="/data/MLDSST/nielsond/target_test/other_repos/Liston-Laboratory-MultiEchofMRI-Pipeline/MultiEchofMRI-Pipeline"
+
+# check to see if there's a symlink to res0urces here
+if ! [[ -e ${MEDIR}/res0urces ]]; then
+    ln -s ${MEDIR}/../Res0urces ${MEDIR}/res0urces
+fi
 
 # these variables should not be changed unless you have a very good reason
 DOF=6 # this is the degrees of freedom (DOF) used for SBref --> T1w and EPI --> SBref coregistrations;
@@ -36,7 +50,7 @@ MaxIterations=500
 MaxRestarts=5
 
 # set variable value that sets up environment
-EnvironmentScript="/home/charleslynch/HCPpipelines-master/Examples/Scripts/SetUpHCPPipeline.sh" # Pipeline environment script
+EnvironmentScript="/data/MLDSST/nielsond/target_test/other_repos/HCPpipelines/Examples/Scripts/SetUpHCPPipeline.sh" # Pipeline environment script; users need to set this 
 source ${EnvironmentScript}	# Set up pipeline environment variables and software
 
 echo -e "\nMulti-Echo Preprocessing & Denoising Pipeline" 
@@ -45,39 +59,53 @@ echo -e "\nProcessing the Field Maps"
 
 # process all field maps & create an average image 
 # for cases where scan-specific maps are unavailable;
+echo "$MEDIR"/func_preproc_fm.sh "$MEDIR" "$Subject" \
+"$StudyFolder" "$NTHREADS" "$StartSession"
+
 "$MEDIR"/func_preproc_fm.sh "$MEDIR" "$Subject" \
 "$StudyFolder" "$NTHREADS" "$StartSession"
 
 echo -e "Coregistering SBrefs to the Anatomical Image"
-
+echo "$MEDIR"/func_preproc_coreg.sh "$MEDIR" "$Subject" "$StudyFolder" \
+"$AtlasTemplate" "$DOF" "$NTHREADS" "$StartSession"
 # create an avg. sbref image and co-register that 
-# image & all individual SBrefs to the T1w image;
+# image & all individual SBrefs to the T1w image;AtlasSpace
 "$MEDIR"/func_preproc_coreg.sh "$MEDIR" "$Subject" "$StudyFolder" \
 "$AtlasTemplate" "$DOF" "$NTHREADS" "$StartSession"
 
 echo -e "Correcting for Slice Time Differences, Head Motion, & Spatial Distortion"
 
 # correct func images for slice time differences and head motion;
+echo "$MEDIR"/func_preproc_headmotion.sh "$MEDIR" "$Subject" "$StudyFolder" \
+"$AtlasTemplate" "$DOF" "$NTHREADS" "$StartSession"
+
 "$MEDIR"/func_preproc_headmotion.sh "$MEDIR" "$Subject" "$StudyFolder" \
 "$AtlasTemplate" "$DOF" "$NTHREADS" "$StartSession"
 
 echo -e "Performing Signal-Decay Based Denoising"
 
 # perform signal-decay denoising; 
+echo "$MEDIR"/func_denoise_meica.sh "$Subject" "$StudyFolder" "$NTHREADS" \
+"$MEPCA" "$MaxIterations" "$MaxRestarts" "$StartSession"
+
 "$MEDIR"/func_denoise_meica.sh "$Subject" "$StudyFolder" "$NTHREADS" \
 "$MEPCA" "$MaxIterations" "$MaxRestarts" "$StartSession"
 
 echo -e "Removing Spatially Diffuse Noise via MGTR"
 
 # remove spatially diffuse noise; 
+echo "$MEDIR"/func_denoise_mgtr.sh "$Subject" \
+"$StudyFolder" "$MEDIR" "$StartSession"
+
 "$MEDIR"/func_denoise_mgtr.sh "$Subject" \
 "$StudyFolder" "$MEDIR" "$StartSession"
 
 echo -e "Mapping Denoised Functional Data to Surface"
 
 # volume-to-surface + spatial smoothing mapping;
-"$MEDIR"/func_vol2surf.sh "$Subject" "$StudyFolder" \
+echo "$MEDIR"/func_vol2surf.sh "$Subject" "$StudyFolder" \
 "$MEDIR" "$CiftiList" "$StartSession"
 
-
+"$MEDIR"/func_vol2surf.sh "$Subject" "$StudyFolder" \
+"$MEDIR" "$CiftiList" "$StartSession"
 
