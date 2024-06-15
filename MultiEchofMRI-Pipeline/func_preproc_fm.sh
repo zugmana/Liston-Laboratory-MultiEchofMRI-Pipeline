ca37556e98c53161b5bd365beb06f549d5421d94
zugmana <andre.zugman@nih.gov>
2024-06-15 10:16:53 -0400

    f

--- MultiEchofMRI-Pipeline/func_preproc_fm.sh
+++ MultiEchofMRI-Pipeline/func_preproc_fm.sh
@@ -60,7 +60,7 @@ done
 # define a list of directories;
 AllFMs=$(cat "$Subdir"/AllFMs.txt) # note: this is used for parallel processing purposes.
 echo ${ALLFMs}
-#rm "$Subdir"/AllFMs.txt # remove intermediate file;
+rm "$Subdir"/AllFMs.txt # remove intermediate file;
 
 # # create a white matter segmentation (.mgz --> .nii.gz);
 mri_binarize --i "$Subdir"/anat/T1w/"$Subject"/mri/aparc+aseg.mgz --wm --o "$Subdir"/anat/T1w/"$Subject"/mri/white.mgz # > /dev/null 2>&1  
@@ -124,7 +124,7 @@ func () {
 }
 
 export -f func # create a field map for all sessions (if possible)
-parallel --jobs $NTHREADS --shellquote bash func ::: $MEDIR ::: $Subdir ::: $Subject ::: $WDIR ::: $AllFMs # > /dev/null 2>&1  
+parallel --jobs $NTHREADS func ::: $MEDIR ::: $Subdir ::: $Subject ::: $WDIR ::: $AllFMs 
 
 # merge & average the co-registered field map images accross sessions;  
 fslmerge -t "$Subdir"/func/field_maps/Avg_FM_rads_acpc.nii.gz "$WDIR"/FM_rads_acpc_S*.nii.gz 
