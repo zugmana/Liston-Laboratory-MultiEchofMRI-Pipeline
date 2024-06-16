060025d8dddc5991188c33efc80981afcef131b4
zugmana <andre.zugman@nih.gov>
2024-06-16 01:43:12 -0400

    added nojvm to matlab call

--- MultiEchofMRI-Pipeline/func_smooth_subcort_concat.sh
+++ MultiEchofMRI-Pipeline/func_smooth_subcort_concat.sh
@@ -18,8 +18,8 @@ cp -rf "$MEDIR"/Res0urces/smooth_subcort_concat.m \
 echo "addpath(genpath('/opt/Liston-Laboratory-MultiEchofMRI-Pipeline/Res0urces/jsonlab')); addpath(genpath('${MEDIR}'))" | cat - "$Subdir"/workspace/temp.m > temp && mv temp "$Subdir"/workspace/temp.m
 echo Subdir=["'$Subdir'"] | cat - "$Subdir"/workspace/temp.m >> temp && mv temp "$Subdir"/workspace/temp.m # > /dev/null 2>&1 		
 cd "$Subdir"/workspace/ # run script via Matlab
-Echo "Starting Matlab script
-matlab -nodesktop -nosplash -r "temp; exit" # > /dev/null 2>&1
+Echo "Starting Matlab script"
+matlab -nodesktop -nosplash -nojvm -r "temp; exit" # > /dev/null 2>&1
 echo "Done"
 
 # delete some files;
