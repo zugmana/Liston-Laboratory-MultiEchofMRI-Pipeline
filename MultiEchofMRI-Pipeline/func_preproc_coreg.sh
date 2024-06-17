9c5dc1c970d278a480f44a7baff0f3e0de3054a7
zugmana <andre.zugman@nih.gov>
2024-06-16 20:51:10 -0400

    removeved preproc_coreg script

--- MultiEchofMRI-Pipeline/func_preproc_coreg.sh
+++ MultiEchofMRI-Pipeline/func_preproc_coreg.sh
@@ -254,16 +254,16 @@ cd "$Subdir" # go back to subject dir.
 # Obs: This sometimes cause the container to hang forever or throw a java error. Will turn off the movie.
 # Probably this is a problem with running the container in a Mac
 # fresh workspace dir.
-rm -rf "$Subdir"/workspace/ > /dev/null 2>&1
-mkdir "$Subdir"/workspace/ > /dev/null 2>&1
+#rm -rf "$Subdir"/workspace/ > /dev/null 2>&1
+#mkdir "$Subdir"/workspace/ > /dev/null 2>&1
 
 # create temporary CoregQA.m 
-cp -rf "$MEDIR"/res0urces/coreg_qa.m \
-"$Subdir"/workspace/temp.m
+#cp -rf "$MEDIR"/res0urces/coreg_qa.m \
+#"$Subdir"/workspace/temp.m
 
 # define some Matlab variables;
-echo "addpath(genpath('${MEDIR}'))" | cat - "$Subdir"/workspace/temp.m > temp && mv temp "$Subdir"/workspace/temp.m
-echo Subdir=["'$Subdir'"] | cat - "$Subdir"/workspace/temp.m >> temp && mv temp "$Subdir"/workspace/temp.m # > /dev/null 2>&1 		
-cd "$Subdir"/workspace/ # run script via Matlab 
-matlab -nodesktop -nosplash -nojvm  -r "temp; exit" # > /dev/null 2>&1
-rm -rf "$Subdir"/workspace/ > /dev/null 2>&1
+#echo "addpath(genpath('${MEDIR}'))" | cat - "$Subdir"/workspace/temp.m > temp && mv temp "$Subdir"/workspace/temp.m
+#echo Subdir=["'$Subdir'"] | cat - "$Subdir"/workspace/temp.m >> temp && mv temp "$Subdir"/workspace/temp.m # > /dev/null 2>&1 		
+#cd "$Subdir"/workspace/ # run script via Matlab 
+#matlab -nodesktop -nosplash -nojvm  -r "temp; exit" # > /dev/null 2>&1
+#rm -rf "$Subdir"/workspace/ > /dev/null 2>&1
